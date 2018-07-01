66238fbb841b7294d18a5727094b30fc39b1b0a1
Sreeyesh A <sreeyesh009@gmail.com>
2018-07-01 19:20:40 +0530

    Added Login Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
   root 'home#index'
+
+  match 'accounts/login', :to => "accounts#login", :as => "login", :via => [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
