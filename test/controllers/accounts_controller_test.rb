2b6a7b0f4f0feaaa9d48c9e07c8c0e700142922c
Sreeyesh A <sreeyesh009@gmail.com>
2018-07-01 19:30:00 +0530

    Added Login Page

---
+++ test/controllers/accounts_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class AccountsControllerTest < ActionDispatch::IntegrationTest
+  test "should get login" do
+    get accounts_login_url
+    assert_response :success
+  end
+
+end
